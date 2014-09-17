1e9fdbbd5467bc1b5a8d90c942c39b50ae5cef5f
Juan Santa Cruz <jhsantacruz@icloud.com>
2014-09-16 23:09:53 -0700

    Added Friends and Inbox fragments with the corresponding view pager which is the container for the fragments.

---
+++ app/src/main/java/com/juanhsantacruz/ribbit/InboxFragment.java
@@ -0,0 +1,21 @@
+package com.juanhsantacruz.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by xmortumx on 9/16/14.
+ */
+public class InboxFragment extends ListFragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+
+        return rootView;
+    }
+}
