de45b412fd233cd7c71dd2b8eeb6c273f64d0967
Juan Santa Cruz <jhsantacruz@icloud.com>
2014-09-21 16:25:56 -0700

    Added the capability to start a new intent and fire the camera/video intent.... writing the files back into Ribbit directory, and broadcasting back to the Gallery the images/video.

--- app/src/main/java/com/juanhsantacruz/ribbit/RebbitMainActivity.java
+++ app/src/main/java/com/juanhsantacruz/ribbit/RebbitMainActivity.java
@@ -1,9 +1,14 @@
 package com.juanhsantacruz.ribbit;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -11,14 +16,118 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class RebbitMainActivity extends FragmentActivity implements ActionBar.TabListener {
 
     public static final String TAG = RebbitMainActivity.class.getSimpleName();
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int TAKE_VIDEO_REQUEST = 1;
+    public static final int PICK_PHOTO_REQUEST = 2;
+    public static final int PICK_VIDEO_REQUEST = 3;
+
+    public static final int MEDIA_TYPE_IMAGE = 4;
+    public static final int MEDIA_TYPE_VIDEO= 5;
+
+    protected Uri mMediaUri;
+
+    protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            switch(i){
+                case 0: //Take picture
+                    Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    mMediaUri = getOutputMediaFIelUri(MEDIA_TYPE_IMAGE);
+                    if(mMediaUri == null){
+                        Toast.makeText(RebbitMainActivity.this, R.string.error_external_storage,
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                        startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+                    }
+                    break;
+                case 1: // Take video
+                    Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                    mMediaUri = getOutputMediaFIelUri(MEDIA_TYPE_VIDEO);
+                    if(mMediaUri == null){
+                        Toast.makeText(RebbitMainActivity.this, R.string.error_external_storage,
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        videoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                        videoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10);
+                        videoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+                        startActivityForResult(videoIntent, TAKE_VIDEO_REQUEST);
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+        private Uri getOutputMediaFIelUri(int mediaTypeImage) {
+            if(isExternalStorageAvailable()){
+                // get the URI
+
+                // 1) Get the external storage directory
+                String appName = RebbitMainActivity.this.getString(R.string.app_name);
+
+                File mediaStorageDirectory = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                        appName);
+                // 2) Create our subdirectory
+                if(! mediaStorageDirectory.exists()){
+                    if(! mediaStorageDirectory.mkdirs()){
+                        Log.e(TAG, "Failed to create directory!");
+                        return null;
+                    }
+                }
+
+                // 3) Create a file name
+                File mediaFile;
+                Date now = new Date();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(now);
+
+                String path = mediaStorageDirectory.getPath() + File.separator;
+
+                // 4) Create the file
+                if(mediaTypeImage == MEDIA_TYPE_IMAGE){
+                    mediaFile = new File(path + "IMG_" + timeStamp + ".jpg");
+                } else if (mediaTypeImage == MEDIA_TYPE_VIDEO){
+                    mediaFile = new File(path + "VID_" + timeStamp + ".mp4");
+                } else {
+                    return null;
+                }
+
+                Log.d(TAG, "File: " + Uri.fromFile(mediaFile));
+                // 5) return the file's URI
+                return Uri.fromFile(mediaFile);
+            } else {
+                return null;
+            }
+        }
+
+        private boolean isExternalStorageAvailable(){
+            String state = Environment.getExternalStorageState();
+
+            if(state.equals(Environment.MEDIA_MOUNTED)){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+
+
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -86,6 +195,21 @@ public class RebbitMainActivity extends FragmentActivity implements ActionBar.Ta
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            // add it to the Galerry
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            mediaScanIntent.setData(mMediaUri);
+            sendBroadcast(mediaScanIntent);
+
+        } else if (resultCode != RESULT_CANCELED) {
+            Toast.makeText(this, "Sorry, there was an error!", Toast.LENGTH_LONG).show();
+        }
+    }
+
     private void navigateToLogin() {
         Intent intent = new Intent(this, LoginActivity.class);
         // Logging in should be a new Task
@@ -118,6 +242,12 @@ public class RebbitMainActivity extends FragmentActivity implements ActionBar.Ta
             case R.id.action_edit_friends:
                 Intent intent = new Intent(this, EditFriendsActivity.class);
                 startActivity(intent);
+                return true;
+            case R.id.action_camera:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(R.array.camera_choices, mDialogListener);
+                AlertDialog dialog = builder.create();
+                dialog.show();
                 return true;
         }
         return super.onOptionsItemSelected(item);
