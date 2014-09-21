2eb4e02f2fd70a15175b57e66d78fefa0884168a
Juan Santa Cruz <jhsantacruz@icloud.com>
2014-09-21 13:49:24 -0700

    Implemented the storing of friends Parse relationship

--- app/src/main/java/com/juanhsantacruz/ribbit/EditFriendsActivity.java
+++ app/src/main/java/com/juanhsantacruz/ribbit/EditFriendsActivity.java
@@ -6,31 +6,42 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
 
 public class EditFriendsActivity extends ListActivity {
     public static  final String TAG = EditFriendsActivity.class.getSimpleName();
-    protected  List<ParseUser> mUsers;
+    protected List<ParseUser> mUsers;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_friends);
+
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
 
     @Override
     protected  void onResume() {
         super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
 
         setProgressBarIndeterminateVisibility(true);
         ParseQuery<ParseUser> query = ParseUser.getQuery();
@@ -88,5 +99,25 @@ public class EditFriendsActivity extends ListActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        if(getListView().isItemChecked(position)) {
+            // add friend
+            mFriendsRelation.add(mUsers.get(position));
+            mCurrentUser.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e != null) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                }
+            });
+        } else {
+            // remove friend
+        }
     }
 }
